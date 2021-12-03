3ffa007737d08581d25259daa67afb7eff4e3e7b
ethelpruss <47779830+ethelpruss@users.noreply.github.com>
2021-12-03 12:57:04 +0100

    Adding server connection

--- deployment/Orchestrator_server.py
+++ deployment/Orchestrator_server.py
@@ -1,14 +1,15 @@
 ## Orchestrator Component - GSN ##
+import Interface_GUI
+from subprocess import call
 
-import AI
-import Interface
+# Get AI model from server
+# Navigate to pscp.exe location
+cmd = "cd C:\Program Files\PuTTY"
+call(cmd)
+# Request trained chat model data from our SSH server using pscp
+cmd = "pscp Administrator@DESKTOP-9M3DEH6:C:/Users/Administrator/Desktop/Moviebot/chat_model.h5 C:\Users\Ethel\Desktop\Moviebot"
+call(cmd)
 
-## Train AI ##
-AI.trainAI()
-
-## Command Line Version - uncomment to run ##
-#Interface.chat() 
-
-## GUI Version ## 
-Interface.chatbox()
+## GUI interface ## 
+Interface_GUI.chatbox()
 
